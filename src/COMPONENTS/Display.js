a0d0c05824cf40a7c7f52d2b4c7b4b01ca92666b
Nagendra Shrestha <nagen.671@gmail.com>
2022-03-20 14:38:51 +1100

    first

---
+++ src/COMPONENTS/Display.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Display = () => {
+  return (
+    <div className="result" id="result">0.00</div>
+  );
+}
---
+++ src/COMPONENTS/Title.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Title = () => {
+  return (
+    <h1>REACT Prank calacultor🙉</h1>
+  )
+}
